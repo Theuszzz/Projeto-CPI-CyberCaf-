fc0b7c77742494c3e7b460ff7567696e044249fa
CyberPuto <matheus.fernandes.r10@gmail.com>
2024-08-07 18:19:29 -0300

    "Formulario atualizado"

--- components/formCompra.js
+++ components/formCompra.js
@@ -2,36 +2,35 @@ const formCompraComponent = {
     template: 
     /*html*/`
     <form id="order-form">
-    
-    <fieldset>
-      <legend>Dados de Identificação</legend>
-      <label for="name">Nome:</label>
-      <input type="text" id="name" name="name" required><br>
+  <fieldset>
+    <legend>Dados de Identificação</legend>
+    <label for="name">Nome:</label>
+    <input type="text" id="name" name="name" required><br>
 
-      <label for="email">Email:</label>
-      <input type="email" id="email" name="email" required><br>
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required><br>
 
-      <label for="phone">Telefone:</label>
-      <input type="tel" id="phone" name="phone" required><br>
-    </fieldset>
+    <label for="phone">Telefone:</label>
+    <input type="tel" id="phone" name="phone" required><br>
+  </fieldset>
 
-    
+  <div class="flex-container">
     <fieldset>
       <legend>Produtos</legend>
-      <label for="product1">Produto 1:</label>
-      <input type="text" id="product1" name="product1" placeholder="Nome do Produto"><br>
+      <label for="machines">Máquinas:</label>
+      <select id="machines" name="machines">
+        <option value="">Selecione...</option>
+      </select><br>
 
-      <label for="product2">Produto 2:</label>
-      <input type="text" id="product2" name="product2" placeholder="Nome do Produto"><br>
+      <label for="sachets">Sachês:</label>
+      <select id="sachets" name="sachets">
+        <option value="">Selecione...</option>
+      </select><br>
 
-      <label for="product3">Produto 3:</label>
-      <input type="text" id="product3" name="product3" placeholder="Nome do Produto"><br>
-      
       <label for="additional-products">Produtos Adicionais:</label>
       <textarea id="additional-products" name="additional-products" rows="4" cols="50" placeholder="Descreva outros produtos se necessário"></textarea><br>
     </fieldset>
 
-    
     <fieldset>
       <legend>Formas de Pagamento</legend>
       <label for="payment-method">Método de Pagamento:</label>
@@ -52,9 +51,13 @@ const formCompraComponent = {
       <label for="cvv">CVV:</label>
       <input type="text" id="cvv" name="cvv" placeholder="123" required><br>
     </fieldset>
+  </div>
 
-    <button type="submit">Enviar Pedido</button>
-  </form>
+  <button type="submit">Enviar Pedido</button>
+</form>
+
+
+
     `
 
 }
